d1bb622052423c13ca5f9a1fd04ffaa4e4982197
Danial Afiq <danialafiq.dev@gmail.com>
2026-03-03 03:06:20 +0800

    updated Noble Class: constructor, getId(), getPoints(), getRequirements(), override toString()

--- src/splendor/entities/Noble.java
+++ src/splendor/entities/Noble.java
@@ -1,3 +1,27 @@
-public class Noble {
+package splendor.entities;
 
+import java.util.EnumMap;
+
+public class Noble {
+    private String id;
+    private int points;
+    private EnumMap<GemColor, Integer> requirements;
+    public Noble(String id, int points, EnumMap<GemColor, Integer> requirements) {
+        this.id = id;
+        this.points = points;
+        this.requirements = requirements;
+    }
+    public String getId() {
+        return id;
+    }
+    public int getPoints() {
+        return points;
+    }
+    public EnumMap<GemColor, Integer> getRequirements() {
+        return requirements;
+    }
+    @Override
+    public String toString() {
+        return String.format("Noble{id=%s, points=%d, requirements=%s}", id, points, requirements);
+    }
 }
